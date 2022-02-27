64182b3f755a56cb79101fd68dd5e7c67b89de61
Aleksander <sasja_git@bef.no>
2022-02-27 13:58:38 +0100

    Implement queue code

--- source/queue.h
+++ source/queue.h
@@ -1,9 +1,18 @@
 enum ReqButton { FirstFloor = 1, SecondFloor = 2, ThirdFloor = 3, FourthFloor = 4, Up = 5, Down = 6}
 enum Location { FirstFloor = 1, SecondFloor = 2, ThirdFloor = 3, FourthFloor = 4}
 
+// Macros for simpler code
+#define nullptr ((void*)0)
 
 struct Request {
-    enum ReqButton dest;
+    enum ReqButton but;
     enum Location loc;
 
 }
+
+void EnqueueFront(Request* req);
+
+void EnqueueBack(Request* req);
+
+Request* Dequeue();
+
