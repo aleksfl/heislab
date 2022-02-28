08c20dcabcfae95ecad2c0d9434e51ec95c6157c
Aleksander Klund <alekskl@stud.ntnu.no>
2022-02-28 17:10:51 +0100

    added createRequest()

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include "driver/elevio.h"
 #include "elevator.h"
+#include "request.h"
 
 
 
@@ -31,6 +32,9 @@ int main(){
         for(int f = 0; f < N_FLOORS; f++){
             for(int b = 0; b < N_BUTTONS; b++){
                 int btnPressed = elevio_callButton(f, b);
+                if(btnPressed) {
+                    createRequest(f, b, floor);
+                }
                 elevio_buttonLamp(f, b, btnPressed);
             }
         }
--- source/queue.h
+++ source/queue.h
@@ -1,3 +1,5 @@
+#pragma once
+
 enum Button { FirstFloor = 1, SecondFloor = 2, ThirdFloor = 3, FourthFloor = 4, Up = 5, Down = 6}
 enum Location { FirstFloor = 1, SecondFloor = 2, ThirdFloor = 3, FourthFloor = 4}
 
@@ -7,7 +9,6 @@ enum Location { FirstFloor = 1, SecondFloor = 2, ThirdFloor = 3, FourthFloor = 4
 struct Request {
     enum Button but;
     enum Location loc;
-
 }
 
 void EnqueueFront(Request* req);
