48ad3713effdcbe8e140f9e80f68b7ef8c235424
Aleksander <af@sikom.no>
2022-03-03 12:56:09 +0100

    improve code for posterity2

--- source/main.c
+++ source/main.c
@@ -19,6 +19,7 @@ int main(){
             break;
         }
         case Standby: {        
+            CheckButtons();
             if (elevio_stopButton()) {                       
                 currState = Stop;
             }
