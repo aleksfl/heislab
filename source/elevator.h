73cc48b96157e61192c6f06363b4adf19da79bd0
Aleksander <af@sikom.no>
2022-03-10 09:45:30 +0100

    Document elevator.h part 1

--- source/elevator.h
+++ source/elevator.h
@@ -16,13 +16,31 @@ typedef enum {
     Wait                    
 } ElevState;
 
-
+/**
+* @brief Opens elevator door and turns on door lamp if recquired conditions are met.
+*/
 void TryOpenDoor(void);
+
+/**
+* @brief Closes elevator door and turns off door lamp if recquired conditions are met.
+*/
 void TryCloseDoor(void);
 
+/**
+* @brief Iterates through floor and cab request buttons to store request and turn on button light if they are pressed.
+*/
 void CheckButtons(void);
+
+/**
+* @brief Same as <CheckButtons>"()", but skips checking buttons requesting the current floor.
+* @param[in] currFloor Current floor to ignore requests to.
+*/
 void CheckButtonsWithFloor(int currFloor);
 
+/**
+* @brief Same as <CheckButtons>"()", but skips checking buttons requesting the current floor.
+* @param[in] currFloor Current floor to ignore requests to.
+*/
 void ClearQueue(void);
 
 void RemoveFromQueue(int floor);
